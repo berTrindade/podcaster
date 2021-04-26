17da96250070a021849d0f609a187942e4375488
Bernardo Trindade de Abreu <bernardo-trindade@hotmail.com>
2021-04-26 20:50:40 -0300

    fix: remove use of file extension (js, jsx, ts, tsx)  within the import path

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+export function Header() {
+  return (
+    <header>
+      <img src="/logo.svg" alt="Podcaster" />
+      <p>O melhor para você ouvir, sempre</p>
+      <span>Qui, 26 Abril</span>
+    </header>
+  );
+}
