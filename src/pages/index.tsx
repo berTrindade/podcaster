5a557001bb61760e5a71d5ce3441ec313513d096
Bernardo Trindade de Abreu <bernardo-trindade@hotmail.com>
2021-04-28 09:40:46 -0300

    feat(home): make the home page consume our server data using NextJS SSG(Static Site Generation)
    The home page was initially configured to consume our backend server data using SSG(Static Site
    Generation). Meaning that when we access our application it will be more performatic and from time
    to time, specifically every 8 hours, it will a proccess called revalidation and our app will make a
    api call to our backend. And not final but I had also add a eslint rule for enabling the use of
    console.log.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,22 @@
-export default function Home() {
+export default function Home(props) {
+  const { episodes } = props;
+
+  console.log(episodes);
+
   return (
     <h1>Index</h1>
   );
+}
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes');
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+    // Every 8 hours (which means that 3 api calls are going to be made by day)
+    revalidate: 60 * 60 * 8,
+  };
 }
