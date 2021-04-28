a94fc8168cb98dd72fa7566f7d051ff5fdd4218d
Bernardo Trindade de Abreu <bernardo-trindade@hotmail.com>
2021-04-28 12:38:53 -0300

    feat: format backend data right after the request is answered and it will utils folder
    The duration property of the episode object it will be formatted with the helper function called
    'convertDurationToTimeString' so it cant be used in the progress bar of the Player component. Also
    the published_at property was renamed to the camelCase format, so it was  written as 'publishedAt'.
    Plus, the publishedAt property got formatted by two functions of the 'date-fns' package: format and
    parseISO, in order to the result format be '9 abr 20' (e.g).

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,19 @@
 import { GetStaticProps } from 'next';
+import { format, parseISO } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 import { api } from '../services/api';
+import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
 
 type Episode = {
     id: string;
     title: string;
+    thumbnail: string;
+    description: string;
     members: string;
-    // ...
+    duration: number;
+    durationAsString: string;
+    url: string;
+    publishedAt: string;
 }
 
 type HomeProps = {
@@ -31,9 +39,21 @@ export const getStaticProps: GetStaticProps = async () => {
     },
   });
 
+  const episodes = data.map((episode) => ({
+    id: episode.id,
+    title: episode.title,
+    thumbnail: episode.thumbnail,
+    members: episode.members,
+    publishedAt: format(parseISO(episode.published_at), 'd MMM yy', { locale: ptBR }),
+    duration: Number(episode.file.duration),
+    durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
+    description: episode.description,
+    url: episode.file.url,
+  }));
+
   return {
     props: {
-      episodes: data,
+      episodes,
     },
     // Every 8 hours (which means that 3 api calls are going to be made by day)
     revalidate: 60 * 60 * 8,
